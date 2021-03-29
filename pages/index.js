5f3ee2c1c389f35f3f63fd8b4c334e9eaedb0fcb
Vamshi Krishna Alladi <vamshikrishnaalladi.me@gmail.com>
2021-03-29 20:41:17 +0530

    chore: setup a next.js project scaffold

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Vamshi Krishna Alladi's Blog</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <h1>
+                    My Blog!
+                </h1>
+            </main>
+        </div>
+    )
+}
